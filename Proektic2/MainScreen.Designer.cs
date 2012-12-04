c33a6437a3438032cd66246659fda0531605b05a
Alexanderkz <leskgraf@gmail.com>
2012-12-04 12:18:04 +0600

    Клик мышки

--- Proektic2/MainScreen.Designer.cs
+++ Proektic2/MainScreen.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Proektic2
 {
-    partial class Form1
+    partial class MainScreen
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Name = "MainScreen";
             this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainScreen_MouseDown);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Proektic2/Program.cs
+++ Proektic2/Program.cs
@@ -14,7 +14,7 @@ namespace Proektic2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainScreen());
         }
     }
 }
